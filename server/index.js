b4ae3fd425861a8a256beb0f727494f2ba495211
JonathanWaller <waller.j27@gmail.com>
2018-07-31 22:07:16 -0500

    endpoint test

--- server/index.js
+++ server/index.js
@@ -1,9 +1,21 @@
+require("dotenv").config();
 const express = require("express");
 const { json } = require("body-parser");
-const PORT = 3001;
+const axios = require("axios");
+const massive = require("massive");
+const PORT = process.env.port || 3001;
 
 const app = express();
 
 app.use(json());
+
+massive(process.env.CONNECTION_STRING).then(dbInstance => {
+  return app.set("db", dbInstance);
+});
+
+app.get("/api/test", (req, res) => {
+  console.log("testing this apiiii");
+  res.status(200).json("tessssstttting");
+});
 
 app.listen(PORT, () => console.log(`hey, listening on port ${PORT}`));
